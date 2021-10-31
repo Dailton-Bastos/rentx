f0af1cf9c46e39538ca2aa5107df04e6260c9b40
Dailton Bastos <dailtonbastos@gmail.com>
2021-10-31 20:47:42 -0300

    feat: init catalog pag

---
+++ src/pages/Catalog/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../../components/Header';
+import { Navigation } from '../../components/Nav';
+
+export const Catalog = (): JSX.Element => {
+	return (
+		<>
+			<Header title='Início' />
+			<Navigation />
+			<div>Catalog</div>
+		</>
+	);
+};
